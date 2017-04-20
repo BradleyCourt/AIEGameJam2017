b0c49f7bb8df6d919f5fbf729c7509ec26f922ab
Daniel Palamountain <s171127@ad.aie.edu>
2017-04-20 11:53:33 +0930

    Wrote a couple of scripts and created Game Over Canvas.

---
+++ Assets/Django's Rubbish/Scripts/KillPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour {
+
+    public GameObject GameOverCanvas;
+
+
+    void start()
+    {
+        GameOverCanvas = GameObject.FindGameObjectWithTag("GameOverCanvas");
+    }
+
+    void OnTriggerEnter(Collider other)
+
+    {
+        if (other.name == "Player")
+
+        {
+            Debug.Log("" + gameObject.name + " KILLED PLAYER");
+            GameOverCanvas.SetActive(true);
+        }
+    }
+}
