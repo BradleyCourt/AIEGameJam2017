197a644fec9923b1337550d2ee8628856e61f045
Daniel Palamountain <s171127@ad.aie.edu>
2017-04-21 12:11:51 +0930

    Changed score value.  Added Main Menu Scene and some folders.

--- Assets/Scripts/ItemAndWallSpawner.cs
+++ Assets/Scripts/ItemAndWallSpawner.cs
@@ -119,7 +119,7 @@ public class ItemAndWallSpawner : MonoBehaviour {
         {
             //Add 1 to score and relocate "Food" item randomly & Spawn a new "Hazard".
             food.transform.position = new Vector3(Random.Range(-4.5f, 4.5f), Random.Range(3.5f, -3.5f), 0);
-            score = score + 1;
+            score = score + 100;
             playerController.Grow();
             hazardSpawner = GetRandomNumber(4);
             //shieldIconSpawner = GetRandomNumber(4);
